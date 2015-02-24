199beeab758518132f3412d6f669099b36dfbfc7
Alexandr Plesovskikh <me@hgen.ru>
2015-02-24 18:32:51 +0500

    +

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,8 @@ $(function() {
         if (command === 'help') {
             term.echo(
                 'Доступные команды:\n' +
-                '  auth\tавторизоваться в системе\n'+
-                '  getgift\tполучить подарок'
+                '  auth     авторизоваться в системе\n'+
+                '  getgift  получить подарок'
             );
         } else if (command === 'auth') {
             var attempts = 1;
@@ -13,18 +13,19 @@ $(function() {
             term.echo(
                 'Авторизация в системе получения подарка\n' +
                 'Нажмите [[b;#0E1;#000]ctrl+D] для выхода из системы авторизации\n' +
-                'Пожалуйста, подтвердите, что вы не робот, а Эля\n' +
-                'Пожалуйста ответьте на несколько вопросов:\n'
+                'Пожалуйста, подтвердите, что вы не робот, а Эля ответив на несколько вопросов.\n'
             );
             term.echo();
             var currentQuestion = 0;
             var questions = [
                 ['Введите номер вашего любимого Доктора', function(c) {return c === '10';}],
-                ['Чье молоко в дозированных сливках?', function(c) {return c.match(/олен/);}]
+                ['Чье молоко в дозированных сливках?', function(c) {return c.indexOf('олен') > -1;}],
+                ['Какие твои любимые конфеты?', function(c) {return c.indexOf('лакр') > -1;}]
             ];
             term.echo(questions[currentQuestion][0]);
             term.push(function(command, term) {
-                if (questions[currentQuestion][1](command)) {
+                console.log(command.toLowerCase());
+                if (questions[currentQuestion][1](command.toLowerCase())) {
                     term.echo('Так бы ответила Эля!');
                     currentQuestion++;
                     if (currentQuestion === questions.length) {
